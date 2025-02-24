71447677fe7e913d3d314170c8c5491d39790c17
TikWei <pohtw-wm22@student.tarc.edu.my>
2025-02-24 15:30:16 +0800

    product slide

--- index.php
+++ index.php
@@ -1,8 +1,31 @@
 <?php include 'header.php'; ?>
 <section id="home" class="hero">
+    <p id="topSales">Top 1 Sales Product</p>
+    <img id="topProduct" src="/images/top1Product.png">
     <h2>Discover the Magic of Collectibles</h2>
     <p>Limited edition art toys and designer figurines.</p>
     <a href="#products" class="btn">Shop Now</a>
+    <script>
+    const arr =[
+        '/images/top1Product.png',
+        '/images/top2Product.png',
+        '/images/top3Product.png',
+        '/images/top4Product.png'
+    ];
+
+    let i = 0;
+
+    $('#topProduct').on('click', e => {
+        i = ++i % arr.length;
+
+    $('#topSales').text(`Top ${i + 1} Sales Product`);
+
+    $('#topProduct')
+        .hide()
+        .prop('src',arr[i])
+        .fadeIn();
+    });
+</script>
 </section>
 
 <section id="products" class="product-grid">
@@ -25,5 +48,4 @@
         <button>Add to Cart</button>
     </div>
 </section>
-
 <?php include 'footer.php'; ?>
